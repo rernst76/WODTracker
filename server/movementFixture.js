5e7c6cfa1a1cf65f47633751df934919e10bf988
Ryan Ernst <rernst76@gmail.com>
2014-12-31 20:31:25 +0000

    Added some movements to movementFixture.

---
+++ server/movementFixture.js
@@ -0,0 +1,77 @@
+if (Movements.find().count() === 0) {
+  Movements.insert({
+    name: 'Back Squat',
+    field1: {
+      label: 'Reps',
+      type: 'number',
+    },
+    field2: {
+      label: 'Weight',
+      type: 'number'
+    }
+  });
+  
+  Movements.insert({
+    name: 'Front Squat',
+    field1: {
+      label: 'Reps',
+      type: 'number',
+    },
+    field2: {
+      label: 'Weight',
+      type: 'number'
+    }
+  });
+  
+  Movements.insert({
+    name: 'Pull-Ups',
+    field1: {
+      label: 'Reps',
+      type: 'number',
+    },
+    field2: {
+      label: 'Scale',
+      type: 'select',
+      options: [
+        'Butterfly',
+        'Kipping',
+        'Strict',
+        'Black Band',
+        'Green Band',
+        'Blue Band',
+        'Red Band',
+        'Purple Band',
+        'Negatives',
+        'Jumping'
+        ]
+    }
+  });
+  
+  Movements.insert({
+    name: 'Push-Ups',
+    field1: {
+      label: 'Reps',
+      type: 'number',
+    },
+    field2: {
+      label: 'Scale',
+      type: 'select',
+      options: [
+        'Box',
+        'Racked Bar'
+        ]
+    }
+  });
+  
+  Movements.insert({
+    name: 'Kettle Bell Swings',
+    field1: {
+      label: 'Reps',
+      type: 'number'
+    },
+    field2: {
+      label: 'Weight',
+      type: 'number'
+    }
+  });
+}
