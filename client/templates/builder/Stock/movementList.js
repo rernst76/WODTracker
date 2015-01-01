8cbed4adf7cfd0a2a494870fcdcd786670eb7897
Ryan Ernst <rernst76@gmail.com>
2015-01-01 06:31:25 +0000

    Deleted auto generated wodtracker.js. Implemented movement filtering. Closes #7.

--- client/templates/builder/Stock/movementList.js
+++ client/templates/builder/Stock/movementList.js
@@ -4,3 +4,13 @@ Template.movementList.helpers({
     return Movements.find({});
   }
 });
+
+Template.movementList.events({
+  "keyup #movementFilter": function(e, template) {
+    var $listItems = $('#movementList li');
+    var re = new RegExp($(e.target).val(), "i"); // "i" means it's case-insensitive
+    $listItems.show().filter(function() {
+      return !re.test($(this).find('p').text());
+    }).hide();
+  }
+});
