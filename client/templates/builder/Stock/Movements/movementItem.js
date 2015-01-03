2bf2391255c6a889cecb25025ca6b936b8cdef81
Ryan Ernst <rernst76@gmail.com>
2015-01-03 23:57:48 +0000

    Added Session variables to store object being dragged and its' type for movement objects.

--- client/templates/builder/Stock/Movements/movementItem.js
+++ client/templates/builder/Stock/Movements/movementItem.js
@@ -1,8 +1,10 @@
 Template.movementItem.events({
   'dragstart': function(e) {
+     // Set effectAllowed so we can drop it
+    e.originalEvent.dataTransfer.effectAllowed = 'all';
     
-  },
-  'dragend': function(e) {
-    
+    // Store object being dragged and type in Session
+    Session.set("dragObject", this);
+    Session.set("dragItemType", "movement");
   }
 });
