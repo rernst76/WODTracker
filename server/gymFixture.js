4d6e2757703fdc814be7ac333baf321fea89d094
Ryan Ernst <rernst76@gmail.com>
2015-03-26 14:14:33 -0400

    Added gyms collection and fixture.

---
+++ server/gymFixture.js
@@ -0,0 +1,35 @@
+if (Gyms.find().count() === 0) {
+	Gyms.insert({
+		name: 'CrossFit Saol',
+	});
+	Gyms.insert({
+		name: 'CrossFit Distinction',
+	});
+	Gyms.insert({
+		name: 'Chagrin Falls CrossFit',
+	});
+	Gyms.insert({
+		name: 'Great Lakes CrossFit',
+	});
+	Gyms.insert({
+		name: 'CrossFit CLE',
+	});
+	Gyms.insert({
+		name: 'CrossFit Nexus',
+	});
+	Gyms.insert({
+		name: 'CrossFit Cadre',
+	});
+	Gyms.insert({
+		name: 'SPC CrossFit',
+	});
+	Gyms.insert({
+		name: 'CrossFit Akron',
+	});
+	Gyms.insert({
+		name: 'Buckeye CrossFit',
+	});
+	Gyms.insert({
+		name: 'Birdtown CrossFit',
+	});
+}
